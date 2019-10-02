55f1946e2b0165303af711b0eb11e509b1b2140a
Никита Кунгурцев <KungurtsevNI@gmail.com>
2019-10-02 12:36:02 +0400

    Создание модульных тестов

---
+++ Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Controllers;
+using Assert = Xunit.Assert;
+
+namespace WebStore.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        private HomeController _controller;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var loggerMoq = new Mock<ILogger<HomeController>>();
+            _controller = new HomeController(loggerMoq.Object);
+        }
+
+        [TestCleanup]
+        public void CleanUp() { }
+
+        [TestMethod]
+        public void Index_Returns_View()
+        {
+            var result = _controller.Index();
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [TestMethod]
+        public void Error404_Throw_ApplicationException()
+        {
+            var result = _controller.NotFoundPage();
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [TestMethod, ExpectedException(typeof(ApplicationException))]
+        public void ThrowException_Throw_NewApplicationException()
+        {
+            _ = _controller.ThrowException();
+        }
+
+        [TestMethod]
+        public void ErrorStatus_404_Redirect_To_NotFoundPage()
+        {
+            var result = _controller.ErrorStatus("404");
+            var redirectToAction = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Null(redirectToAction.ControllerName);
+            Assert.Equal(nameof(HomeController.NotFoundPage), redirectToAction.ActionName);
+        }
+    }
+}
---
+++ Tests/WebStore.Tests/Controllers/WebApiTestControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Assert = Xunit.Assert;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebStore.Controllers;
+using WebStore.Interfaces.Api;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Tests.Controllers
+{
+    [TestClass]
+    public class WebApiTestControllerTests
+    {
+        private WebApiTestController _controller;
+        private string[] _expectedValues = { "1", "2", "3" };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var valueServiceMock = new Mock<IValuesService>();
+            var expectedValues = new[] { "1", "2", "3" };
+            valueServiceMock
+                .Setup(service => service.GetAsync())
+                .ReturnsAsync(_expectedValues);
+
+            _controller = new WebApiTestController(valueServiceMock.Object);
+        }
+
+        [TestMethod]
+        public async Task Index_Method_Returns_View_With_Values()
+        {
+            var result = await _controller.Index();
+            var view_result = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<string>>(view_result.Model);
+
+            Assert.Equal(_expectedValues.Length, model.Count());
+        }
+    }
+}
