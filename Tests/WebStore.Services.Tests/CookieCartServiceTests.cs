b32e68c56932def617ba65bc46ee9666320d90df
Никита Кунгурцев <KungurtsevNI@gmail.com>
2019-10-02 14:45:22 +0400

    Тест для сервиса корзины

---
+++ Tests/WebStore.Services.Tests/CookieCartServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Models;
+using Assert = Xunit.Assert;
+
+namespace WebStore.Services.Tests
+{
+    [TestClass]
+    class CookieCartServiceTests
+    {
+        [TestMethod]
+        public void Cart_Class_ItemsCount_Returns_Correct_Quantity()
+        {
+            const int expected_count = 4;
+            var cart = new Cart
+            {
+                Items = new List<CartItem>
+                {
+                    new CartItem { ProductId = 1, Quantity = 3 },
+                    new CartItem { ProductId = 2, Quantity = 1 }
+                }
+            };
+
+            var result = cart.ItemsCount;
+
+            Assert.Equal(expected_count, result);
+        }
+    }
+}
